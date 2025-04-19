bf4700a2e212b687e6cdeedfa387d1e91dc17b2e
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-04-19 09:57:31 +0530

    feat: improve layout and styling of ProductAnalyticsPage for better responsiveness

--- app/(core)/dashboard/founder/[id]/page.tsx
+++ app/(core)/dashboard/founder/[id]/page.tsx
@@ -2,8 +2,8 @@ import { ProductAnalytics } from '../_components/ProductAnalytics';
 
 const ProductAnalyticsPage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br  p-2  pl-4 pr-4 from-primary/15 via-secondary/25 to-accent/45">
-      <div className="container py-8">
+    <div className="min-h-screen bg-gradient-to-br from-primary/15 via-secondary/25 to-accent/45 px-4 py-6">
+      <div className="max-w-screen-xl mx-auto">
         <ProductAnalytics />
       </div>
     </div>
